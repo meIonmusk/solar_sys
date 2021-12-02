c31f972472c60b77df197b88ee3a663060788a58
Sardor Islamov <isardor0707@gmail.com>
2021-12-02 20:39:11 +0300

    Перерасчитаны формулы движения космических объектов (но они пока что разлетаются)

--- solar_model.py
+++ solar_model.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 # license: GPLv3
+import math
 
 gravitational_constant = 6.67408E-11
 """Гравитационная постоянная Ньютона G"""
@@ -21,10 +22,10 @@ def calculate_force(body, space_objects):
             continue  # тело не действует гравитационной силой на само себя!
         r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
         r = max(r, body.R)
-        FF = gravitational_constant * body.m * obj.m / r ** 2
-        body.Fx =  FF * math.cos(math.atan((obj.y - body.y) / (obj.x - body.x)))
-        body.Fy =  FF * math.sin(math.atan((obj.y - body.y) / (obj.x - body.x)))
-        
+        force = -gravitational_constant * body.m * obj.m / (r ** 2)
+        body.Fx += force * math.cos(math.atan((obj.y - body.y) / (obj.x - body.x)))
+        body.Fy += force * math.sin(math.atan((obj.y - body.y) / (obj.x - body.x)))
+
 def move_space_object(body, dt):
     """Перемещает тело в соответствии с действующей на него силой.
 
@@ -36,20 +37,20 @@ def move_space_object(body, dt):
       # FIXME: Вывести формулы для ускорения, скоростей и координат
     ax = body.Fx / body.m    
     ay = body.Fy / body.m
-    body.Vx = body.Vx + ax * dt
-    body.Vy = body.Vy + ay * dt
-    body.x = body.x + body.Vx * dt + ax * dt * dt / 2
-    body.y = body.y + body.Vy * dt + ay * dt * dt / 2
-    body.T = body.T + dt
+    body.Vx += ax * dt
+    body.Vy += ay * dt
+    body.x += body.Vx * dt
+    body.y += body.Vy * dt
+    # body.T = body.T + dt
     '''
     сохраняем в файл тройки значений в каждый момент времени
     <модуль скорости>   <расстояние от планеты до звезды>  <время>
     '''
-    if body.type == 'planet':
-        if len(objects) == 2:            
-            with open('stats.txt', 'w') as out_file:
-                out_file.write(str((body.Vx**2 + body.Vy**2)**0.5) + ' ' + str(((body.x - star.x)**2 + (body.y - star.y)**2)**0.5) + ' ' + str(body.T)) 
-            
+    # if body.type == 'planet':
+    #     if len(objects) == 2:
+    #         with open('stats.txt', 'w') as out_file:
+    #             out_file.write(str((body.Vx**2 + body.Vy**2)**0.5) + ' ' + str(((body.x - star.x)**2 + (body.y - star.y)**2)**0.5) + ' ' + str(body.T))
+    #
 
 def recalculate_space_objects_positions(space_objects, dt):
     """Пересчитывает координаты объектов.
